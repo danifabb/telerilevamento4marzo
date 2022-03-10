6d3e63addf21dc83e7ac58ea17f0fce81af05ab2
danifabb <100956821+danifabb@users.noreply.github.com>
2022-03-10 17:49:26 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,12 @@
 # questo è il primo script che useremo a lezione
 
+#install.packages("raster")
 library(raster)
+setwd("C:/lab/")
+l2011 <- brick("p224r63_2011.grd")
+l2011
+ 
+# plot
+plot(l2011)
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011, col=cl)
