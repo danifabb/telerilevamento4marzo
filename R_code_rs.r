8c85b8f3c8f0121f9f27bee93732d409a6f8dc7f
danifabb <100956821+danifabb@users.noreply.github.com>
2022-03-24 17:34:02 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -48,3 +48,30 @@ clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
 plot(l2011$B3_sre, col=clr)
 clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(l2011$B4_sre, col=clnir)
+
+install.packages("RStoolbox")
+library(raster)
+setwd("C:/lab/")
+l2011 <- brick("p224r63_2011.grd")
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(l2011$B3_sre, col=clr)
+# plot RGB layers) - vedo immagine come la vede all'occhio umano
+# stretch serve per massimizzare contrato tra i colori
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="lin")
+# uso stretch="hist" per vedere meglio la diversità della vegetazione, i frattali della veg
+plotRGB(l2011, r=3, g=4, b=2, stretch="hist")
+
+# build a multiframe with visible RGB 
+# linear stretch on top of false colours
+# histogram stretch
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="hist")
+l1988 <- brick("p224r63_1988.grd")
+l1988
+par(mfrow=c(2,1))
+plotRGB(l1988, r=4, g=3, b=2, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
