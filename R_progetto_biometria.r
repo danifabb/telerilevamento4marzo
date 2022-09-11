b4634fbb5bcfc94f4f23a6397200f4baadc12c64
danifabb <100956821+danifabb@users.noreply.github.com>
2022-09-11 19:19:26 +0200

    Update R_progetto_biometria.r

--- R_progetto_biometria.r
+++ R_progetto_biometria.r
@@ -336,6 +336,91 @@ cor.test(BCI_env$EnvHet,
          BCI_env$sr,
          alternative = "greater") #p-value = 0.45 e cor = 0.021
 
+#VERSIONE DEFINITIVA
+# provo a caricare il dataframe "BCI_env.txt", tuttavia si verifica un errore: ci sono alcuni elementi mancanti NA 
+# error: line 9 did not have 9 elements; nella riga 9 manca la modalità per la variabile "Precipitation"
+
+# devo quindi specificare alcuni argomenti, affinché R sappia come trattare i valori mancanti
+# sep = " " serve per indicare che il separatore tra i valori è uno spazio vuoto
+# in questo modo, la casella rimane vuota e non c'è slittamento a sinistra delle righe che presentano i dati mancanti
+# l'argomento header = T serve per indicare che la prima riga = intestazione delle colonne
+BCI_env <- read.table("data/BCI_env.txt", sep = " ", header = T) 
+
+# carico la matrice di comunità (comma-separated values)
+BCI <- read.csv("data/BCI.csv")
+View(BCI)
+
+# conto il numero di osservazioni, specie e variabili ambientali
+nrow(BCI) #50 osservazioni = 50 righe
+ncol(BCI) #225 specie = 225 colonne di BCI
+ncol(BCI_env) #9 variabili ambientali = 9 colonne di BCI_env
+
+# carico il pacchetto vegan per avere le informazioni riguardo al dataset
+install.packages("vegan")
+library(vegan)
+?BCI
+# BCI = Barro Colorado Island. Dati raccolti da 50 plot da 1 ettaro ciascuno. Conteggio delle specie di alberi (matrice)
+# BCI_env: 9 variabili associate agli stessi 50 plot
+
+str(BCI_env) #50 osservazioni di 9 variabili
+# le coordinate UTM.EW e UTM.NS e la misura di eterogeneità ambientale sono vettori numerici
+# i valori di precipitazione (mm/anno) e altitudine (m) sono vettori integer
+
+# il vettore character relativo alla formazione geologica è un unico valore "Tb" per tutti i 50 plot   
+unique(BCI_env$Geology) 
+
+# converto i vettori di tipo character in fattori con più livelli 
+
+BCI_env$Age.cat <- factor(BCI_env$Age.cat, 
+                          ordered = T, 
+                          levels = c("c2", "c3")) # fattore ordinato
+
+as.factor(BCI_env$Age.cat) # controllo che la struttura e i livelli siano corretti
+
+BCI_env$Habitat <- factor(BCI_env$Habitat, 
+                          levels = c("Young", "OldSlope", "OldLow", "OldHigh", "Swamp"))
+
+as.factor(BCI_env$Habitat)
+
+BCI_env$Stream <- factor(BCI_env$Stream, 
+                         levels = c("Yes", "No"))
+
+str(BCI) # vettori integer per abbondanza di specie
+
+# controllo che tra i valori unici non ci siano anche spazi vuoti
+unique(BCI_env$Precipitation)
+unique(BCI_env$Elevation)
+unique(BCI_env$UTM.EW) # 10 modalità
+unique(BCI_env$UTM.NS) # 5 modalità
+unique(BCI_env$EnvHet)
+
+summary(BCI_env) 
+# UTM.EW e UTM.NS: media e mediana coincidono 
+# UTM.NS: min 1011569; max 1011969 - plot selezionati a distanze regolari
+# per precipitation e elevation si ha un unico valore
+# EnvHet: min 0.0000; max 0.7264 
+# EnvHet: 1st Qu 0.08; mean 0.31 - distribuzione spostata verso sinistra
+# ci sono 3 NA, che voglio eliminare assieme alle righe (osservazioni) in cui sono presenti
+ 
+summary(BCI) 
+
+#rimozione NA
+index_na <- which(is.na(BCI_env), arr.ind = T) # ottengo un indice delle posizioni dei valori NA nel dataframe
+index_na[ , 1] # seleziono solo la prima colonna della matrice, ovvero quella che elenca le righe che presentano NA
+BCI_env <- na.omit(BCI_env) # elimino le osservazioni con NA nel dataframe
+View(BCI_env)
+BCI <- BCI[-(index_na[ , 1]), ] # elimino le stesse righe nella matrice di comunità
+
+#controllo il numero di osservazioni
+nrow(BCI_env) #47
+nrow(BCI)
+
+#controllo che non ci siano più NA
+anyNA(BCI_env) 
+
+#is.na(BCI_env$Precipitation)
+#sum(is.na(BCI_env))
+
 
 
 
